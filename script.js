ab25f2587a2f5361a503e74dda33c4ec15c73d9b
AndreiWork <uzatsepin@gmail.com>
2021-03-08 13:16:28 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,13 @@
+"use strict";
+
+// const category = 'toys';
+
+// document.write(`https://someurl.com/${category}/5`);
+
+// const user = 'Ivan';
+// alert(`Привет, ${user}`);
+
+const isChecked = true,
+	isClose = false;
+
+console.log(isChecked && isClose);
